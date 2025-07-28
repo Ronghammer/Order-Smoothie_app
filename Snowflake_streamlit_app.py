a01bc559b89bd1352056bc25cabd2a9971349f6d
Rong <114782858+Ronghammer@users.noreply.github.com>
2025-07-28 10:58:01 -0400

    Update Snowflake_streamlit_app.py
    convert request code to message

--- Snowflake_streamlit_app.py
+++ Snowflake_streamlit_app.py
@@ -60,7 +60,7 @@ if Ingredient_list: # if it IS NOT NULL to remove [] when select nothing
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response).json()
 
 
 
